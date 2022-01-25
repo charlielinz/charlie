0d2644c96997f548692024af02533a4ae146faa0
charlielinz <ilovealinlin@gmail.com>
2022-01-26 02:08:23 +0800

    Update page1.js

--- pages/page1.js
+++ pages/page1.js
@@ -7,10 +7,6 @@ const page1 = () => {
           <div className="row-start-2 row-end-7 bg-gray-50">
           </div>
         </div>
-        <div className="col-start-3 col-end-4 grid grid-rows-6 animate-slidein-right">
-          <div className="row-start-1 row-end-2 bg-gray-50"></div>
-          <div className="row-start-2 row-end-7 bg-gray-300"></div>
-        </div>
       </div>
     </>
   );
