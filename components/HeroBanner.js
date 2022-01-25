354145e9fa7aca47804215421d41a1d13e0e451c
charlielinz <ilovealinlin@gmail.com>
2022-01-26 02:51:16 +0800

    test

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -13,7 +13,7 @@ const HeroBanner = () => {
                   This is
                 </div>
                 <div className="text-5xl lg:text-8xl my-8">
-                  <div className="h-8 lg:h-24 flex items-center before:content-['Charlie'] after:animate-name2-fade-in"></div>
+                  <div className="h-8 lg:h-24 flex items-center before:animate-name1-fade-in after:animate-name2-fade-in"></div>
                 </div>
                 <div className="animate-fade-in">Art & Tech</div>
                 <div className="animate-fade-in">Emotional & Rational</div>
