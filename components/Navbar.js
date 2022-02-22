db7b26aa7fbed451d2d9dd41e8b369f58faf599d
charlielinz <ilovealinlin@gmail.com>
2022-02-22 16:05:19 +0800

    little update

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,18 +9,20 @@ const Navbar = () => {
   const [isOpened, setIsOpened] = useState(false);
 
   return (
-    <div className="max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto">
-      <div className="mt-8 flex items-center h-16 gap-10">
-        <div className="text-gray-800 text-2xl font-bold">
-          <Link href="/">Charlie Lin</Link>
-        </div>
-        {windowWidth > 768 ? (
-          <DesktopNavigation />
-        ) : (
-          <div className="ml-auto">
-            <MobileNavigation isOpened={isOpened} setIsOpened={setIsOpened} />
+    <div className="sticky top-0 z-50 w-full bg-gray-50 opacity-90">
+      <div className="max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto">
+        <div className="mt-8 flex items-center h-16 gap-10">
+          <div className=" text-slate-700 text-2xl font-bold">
+            <Link href="/">Charlie Lin</Link>
           </div>
-        )}
+          {windowWidth > 768 ? (
+            <DesktopNavigation />
+          ) : (
+            <div className="ml-auto">
+              <MobileNavigation isOpened={isOpened} setIsOpened={setIsOpened} />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import useWindowWidth from "../hooks/useWindowWidth";
 import HeroBanner from "../components/HeroBanner";
 import Navbar from "../components/Navbar";
+import me from "../public/img/me.jpg";
 
 export default function Home() {
+  const windowWidth = useWindowWidth();
   return (
     <header>
       <Navbar />
-      <div className="relative z-10 pt-32 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto">
-        <HeroBanner />
+      <div className="w-full h-96 bg-gray-300">
+        <div className="pt-28 lg:pt-24 z-10 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto">
+          <HeroBanner />
+        </div>
       </div>
-      <div className="absolute top-24 md:top-28 lg:top-32 w-full h-96 bg-gray-300" />
+      <div className="py-16 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto">
+        <div className="flex md:flex-col-reverse justify-center md:items-end gap-4">
+          <div className="flex md:flex-col items-center gap-1 text-lg md:pr-5">
+            <p>It's my life style.</p>
+            <p className="underline">
+              About
+              <span>
+                <Link href="/about"> Charlie Lin</Link>
+              </span>
+            </p>
+          </div>
+          {windowWidth > 768 ? (
+            <div className="relative w-48 h-48">
+              <Image
+                src={me}
+                alt="me"
+                layout="fill"
+                objectFit="contain"
+                priority={true}
+                className="rounded-full"
+              />
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
     </header>
   );
 }
