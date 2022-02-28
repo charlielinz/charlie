9d703552340c20c3eb1fa9e256436eb952469bfe
charlielinz <ilovealinlin@gmail.com>
2022-02-28 18:17:15 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import useWindowWidth from "../hooks/useWindowWidth";
 import HeroBanner from "../components/HeroBanner";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -14,7 +13,6 @@ import southKorea from "../public/img/south-korea.png";
 import usa from "../public/img/united-states-of-america.png";
 
 export default function Home() {
-  const windowWidth = useWindowWidth();
   return (
     <header>
       <Navbar />
@@ -23,30 +21,26 @@ export default function Home() {
           <HeroBanner />
         </div>
       </div>
-      <div className="flex flex-col-reverse md:flex-row justify-between py-16 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto gap-14">
-        <div className="flex md:flex-col-reverse justify-center md:items-end gap-4">
-          <div className="flex md:flex-col items-center gap-1 text-lg md:pr-5 pt-6 md:pt-0">
+      <div className="flex flex-col md:flex-row justify-between py-16 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-6 md:mx-auto gap-14">
+        <div className="flex md:flex-col justify-center md:items-end gap-4">
+          <div className="relative w-40 lg:w-48 h-40 lg:h-48">
+            <Image
+              src={me}
+              alt="me"
+              layout="fill"
+              objectFit="contain"
+              priority={true}
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-1 text-lg md:pr-5">
             <p className="md:text-base lg:text-xl ">It is my life style.</p>
             <p className="underline md:text-base lg:text-xl">
               <Link href="/about">About Charlie Lin</Link>
             </p>
           </div>
-          {windowWidth > 768 ? (
-            <div className="relative md:w-40 lg:w-48 md:h-40 lg:h-48">
-              <Image
-                src={me}
-                alt="me"
-                layout="fill"
-                objectFit="contain"
-                priority={true}
-                className="rounded-full"
-              />
-            </div>
-          ) : (
-            ""
-          )}
         </div>
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col">
           <p className="justify-self-center md:justify-self-start mx-auto text-4xl md:text-5xl md:leading-tight lg:text-6xl lg:leading-tight xl:text-7xl xl:leading-tight pb-8 font-bold">
             Sharing travel & food guides!
           </p>
