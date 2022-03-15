06ee778c41e3ef143206f31ba0aecece56fd8674
charlielinz <ilovealinlin@gmail.com>
2022-03-16 00:33:40 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -134,8 +134,8 @@ const Home = () => {
           </button>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row gap-8 w-full py-6 md:py-24">
-        <div className="max-w-screen-md mx-6 md:px-6 min-w-[40%]">
+      <div className="flex flex-col md:flex-row gap-8 w-full py-6 md:py-28">
+        <div className="max-w-screen-md ml-6 md:px-6 min-w-[40%]">
           <p className="text-4xl md:text-6xl leading-tight pb-2 font-bold">
             No more struggle with your meals.
           </p>
@@ -151,9 +151,17 @@ const Home = () => {
             </Link>
           </button>
         </div>
-        <div className="max-w-screen-xl min-w-[40%] mx-12">
+        <div className="max-w-screen-xl min-w-[45%] mx-12">
           <div className="bg-slate-100 rounded-xl shadow-lg">
             <div className="p-4 space-y-2">
+              <div className="flex gap-8 py-2">
+                <button className="py-1 px-5 bg-slate-500 text-gray-100 rounded-lg text-lg">
+                  New Post
+                </button>
+                <button className="py-1 px-5 bg-slate-200 rounded-lg text-lg">
+                  Top Rated
+                </button>
+              </div>
               <div className="flex justify-between p-4 border-t-2 border-t-gray-200">
                 <div className="space-y-1">
                   <p className="text-2xl font-bold">Omakase</p>
