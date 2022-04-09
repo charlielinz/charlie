0963b5a24bd16e1d228bc59f3be07e8026a0181c
charlielinz <ilovealinlin@gmail.com>
2022-04-09 13:38:20 +0800

    Update: GA4

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,22 @@ const Document = () => {
           src="https://kit.fontawesome.com/1b3fa5cc6e.js"
           crossOrigin="anonymous"
         />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body className="bg-gray-50">
         <Main />
